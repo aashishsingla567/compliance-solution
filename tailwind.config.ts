c6f063eb2e36421118eb99a717d4971c024c058e
Ayush-kathayat <kathayatayush789@gmail.com>
2024-03-09 19:21:31 +0530

    feat/colors : added css colors variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,31 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      // fontFamily: {
+      //   sans: ["var(--font-sans)", ...fontFamily.sans],
+      // },
+      fontSize: {
+        sm: "0.750rem",
+        base: "1rem",
+        xl: "1.333rem",
+        "2xl": "1.777rem",
+        "3xl": "2.369rem",
+        "4xl": "3.158rem",
+        "5xl": "4.210rem",
+      },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["Cambay", ...fontFamily.sans],
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+      colors: {
+        text: "hsl(var(--text))",
+        background: "hsl(var(--background))",
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        accent: "hsl(var(--accent))",
       },
     },
   },
