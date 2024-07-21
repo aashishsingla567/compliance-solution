f53e18b380e449f8590e7ac87edb59466277e53b
aashishsingla <aashish@goalteller.com>
2024-07-21 15:49:18 +0530

    fix: add svgr

--- src/components/layout/layouts/PreLogin/Header.tsx
+++ src/components/layout/layouts/PreLogin/Header.tsx
@@ -1,10 +1,13 @@
+import Logo from "@components/Logo";
 import MaxWidthContainer from "@components/MaxWidthContainer";
 
 const Header = () => {
   return (
     <MaxWidthContainer className="navbar bg-base-100" as="header">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">
+          <Logo />
+        </a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
